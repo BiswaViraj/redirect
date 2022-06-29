32e70d0ad3f4d536c2d1b340098b2adb9a7bd706
Biswajeet Das <biswajeetdas18@gmail.com>
2022-06-29 14:39:49 +0530

    feat: app done

---
+++ main.js
@@ -0,0 +1,26 @@
+const redirectApp = (url) => {
+  window.location.replace(url);
+};
+const getRedirectUrl = async (shortUrl) => {
+  try {
+    const res = await fetch(`http://localhost:8000/redirect${shortUrl}`);
+
+    const data = await res.json();
+    redirectApp(data.url);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getUrl = async () => {
+  const { pathname } = window.location;
+  console.log(pathname);
+
+  if (pathname === "/") {
+    redirectApp("https://app.miny.com");
+  }
+  const redirectUrl = await getRedirectUrl(pathname);
+  console.log(redirectUrl);
+};
+
+getUrl();
